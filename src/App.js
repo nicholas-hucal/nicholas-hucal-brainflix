7cd5bc3fecd9017a2141543be6c3c2bcd7cb29b5
Nicholas Hucal <nhucal@mac.com>
2022-02-23 17:53:32 -0700

    created VideoDetails component. Completed mobile design portion

--- src/App.js
+++ src/App.js
@@ -3,21 +3,29 @@ import Nav from './components/Nav/Nav';
 import VideoHero from './components/VideoHero/VideoHero';
 import React from 'react';
 import videos from './data/videos.json';
-import videoDetails from './data/video-details.json';
+import VideoDetails from './components/VideoDetails/VideoDetails';
 
 
 class App extends React.Component {
 
   state = {
-    videos: videos,
-    videoDetails: videoDetails
+    videos: videos
   }
 
   render() {
+
+    const formatDateForSite = (timestamp) => {
+      if (timestamp) {
+        return new Date(Number(timestamp)).toLocaleDateString('en-US', { year: 'numeric', month: '2-digit', day: '2-digit' });
+      }
+      return new Date().toLocaleDateString('en-US', { year: 'numeric', month: '2-digit', day: '2-digit' });
+    }
+
     return (
       <>
         <Nav />
         <VideoHero videoSrc="/" videoType="video/mp4" posterSrc={this.state.videos[0].image} />
+        <VideoDetails video={this.state.videos[0]} dateFunction={formatDateForSite} />
       </>
     );
   }
---
+++ src/components/VideoDetails/VideoDetails.js
@@ -0,0 +1,35 @@
+import './VideoDetails.scss';
+import React from 'react';
+import viewIcon from '../../assets/images/icons/views.svg';
+import likeIcon from '../../assets/images/icons/likes.svg';
+import videoDetails from '../../data/video-details.json';
+
+
+class VideoDetails extends React.Component {
+    render() {
+        const currentVideo = videoDetails.find(video => video.id === this.props.video.id);
+
+        return (
+            <section className='video-details'>
+                <h1 className='video-details__title'>{currentVideo.title}</h1>
+                <div className='video-details__info'>
+                    <h3 className='video-details__channel'>By {currentVideo.channel}</h3>
+                    <p className='video-details__date'>{this.props.dateFunction(currentVideo.timestamp)}</p>
+                    <p className='video-details__views'>
+                        <img className='video-details__icon' src={viewIcon} alt="this videos views total" />
+                        {currentVideo.views}
+                    </p>
+                    <p className='video-details__likes'>
+                        <img className='video-details__icon' src={likeIcon} alt="this videos likes total" />
+                        {currentVideo.likes}
+                    </p>
+                </div>
+                <p className='video-details__description'>
+                    {currentVideo.description};
+                </p>
+            </section>
+        );
+    }
+}
+
+export default VideoDetails;
