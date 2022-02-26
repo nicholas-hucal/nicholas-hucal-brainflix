4525f61d6436b269bc06ea53bb123b2217bcde4a
Nicholas Hucal <nhucal@mac.com>
2022-02-25 20:22:22 -0700

    completed commentForm component

--- src/components/CommentForm/CommentForm.js
+++ src/components/CommentForm/CommentForm.js
@@ -1,8 +1,28 @@
 import './CommentForm.scss'
+import buttonImg from '../../assets/images/icons/add_comment.svg';
+import avatar from '../../assets/images/images/Mohan-muruge.jpg';
+import Button from '../Button/Button';
 
-function CommentForm() {
+function CommentForm({commentsCount}) {
   return (
-    <div>CommentForm</div>
+    <section className='comment-form'>
+        <h3 className='comment-form__heading'>{commentsCount} Comments</h3>
+        <div className='comment-form__form-container'>
+            <figure className='comment-form__avatar-container'>
+                <img className='comment-form__avatar' src={avatar} alt="avatar for Mohan Muruge" />
+            </figure>
+            <form className='comment-form__form'>
+                <h5 className='comment-form__form-heading'>Join the conversation</h5>
+                <textarea className='comment-form__textarea' name="comment" placeholder='Add a new comment'></textarea>
+                <Button
+                    className="comment-form__submit"
+                    image={buttonImg}
+                    text="COMMENT"
+                    type="submit"
+                />
+            </form>
+        </div>
+    </section>
   )
 }
 
