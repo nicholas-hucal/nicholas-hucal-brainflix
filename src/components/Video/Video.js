4efa1c0beed0ec2e91f47ca8fe859a330d64dfe9
Nicholas Hucal <nhucal@mac.com>
2022-02-26 17:05:32 -0700

    completed truncate feature

--- src/components/Video/Video.js
+++ src/components/Video/Video.js
@@ -8,7 +8,14 @@ function Video({ video, clickHandler }) {
                 <img className='video__image' src={image} alt={title} />
             </div>
             <div className='video__details'>
-                <p className='video__title'>{title}</p>
+                <p className='video__title'>
+                    <span className='video__title-content video__title-content--full'>
+                        {title}
+                    </span>
+                    <span className='video__title-content video__title-content--truncate'>
+                        {title.substr(0, 33) + "\u2026"}
+                    </span>
+                </p>
                 <p className='video__channel'>{channel}</p>
             </div>
         </article>
